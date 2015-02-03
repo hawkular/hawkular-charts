62100f3f359f5a4b951baaa82d5378b93ce22f85
Mike Thompson <mithomps@redhat.com>
2015-02-03 14:45:47 -0800

    HAWKULAR-22: Migrate rhq metrics charts to Hawkular-charts.

---
+++ src/rhq-metrics-charts.module.js
@@ -0,0 +1,9 @@
+/**
+ * @name  hawkular-charts
+ *
+ * @description
+ *   Base module for rhq-metrics-charts.
+ *
+ */
+angular.module('hawkular-charts', ['d3', 'd3-tip', 'moment']);
+//# sourceMappingURL=rhq-metrics-charts.module.js.map
---
+++ vendor/vendor.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="angular/angular.d.ts" />
+/// <reference path="angular/angular-ui-bootstrap.d.ts" />
+/// <reference path="moment/moment.d.ts" />
