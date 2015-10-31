29ac1195ee4afdae0fe5ce0a37f06a85132092a5
Mike Thompson <mithomps@redhat.com>
2015-10-30 19:36:55 -0700

    Publish lastest artifacts to dist/*

--- hawkular-charts.js
+++ hawkular-charts.js
@@ -7,43 +7,6 @@
  */
 angular.module('hawkular.charts', []);
 
-///
-/// Copyright 2015 Red Hat, Inc. and/or its affiliates
-/// and other contributors as indicated by the @author tags.
-///
-/// Licensed under the Apache License, Version 2.0 (the "License");
-/// you may not use this file except in compliance with the License.
-/// You may obtain a copy of the License at
-///
-///    http://www.apache.org/licenses/LICENSE-2.0
-///
-/// Unless required by applicable law or agreed to in writing, software
-/// distributed under the License is distributed on an "AS IS" BASIS,
-/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-/// See the License for the specific language governing permissions and
-/// limitations under the License.
-///
-var Charts;
-(function (Charts) {
-    'use strict';
-    /// NOTE: this pattern is used because enums cant be used with strings
-    var EventNames = (function () {
-        function EventNames(value) {
-            this.value = value;
-            // empty
-        }
-        EventNames.prototype.toString = function () {
-            return this.value;
-        };
-        EventNames.CHART_TIMERANGE_CHANGED = new EventNames('ChartTimeRangeChanged');
-        EventNames.AVAIL_CHART_TIMERANGE_CHANGED = new EventNames('AvailChartTimeRangeChanged');
-        EventNames.REFRESH_CHART = new EventNames('RefreshChart');
-        EventNames.REFRESH_AVAIL_CHART = new EventNames('RefreshAvailabilityChart');
-        return EventNames;
-    })();
-    Charts.EventNames = EventNames;
-})(Charts || (Charts = {}));
-
 /// <reference path='../../vendor/vendor.d.ts' />
 var Charts;
 (function (Charts) {
@@ -57,9 +20,9 @@ var Charts;
         AvailStatus.prototype.toString = function () {
             return this.value;
         };
-        AvailStatus.UP = new Charts.EventNames('up');
-        AvailStatus.DOWN = new Charts.EventNames('down');
-        AvailStatus.UNKNOWN = new Charts.EventNames('unknown');
+        AvailStatus.UP = 'up';
+        AvailStatus.DOWN = 'down';
+        AvailStatus.UNKNOWN = 'unknown';
         return AvailStatus;
     })();
     Charts.AvailStatus = AvailStatus;
@@ -416,6 +379,43 @@ var Charts;
     })();
     Charts.AvailabilityChartDirective = AvailabilityChartDirective;
     _module.directive('availabilityChart', AvailabilityChartDirective.Factory());
+})(Charts || (Charts = {}));
+
+///
+/// Copyright 2015 Red Hat, Inc. and/or its affiliates
+/// and other contributors as indicated by the @author tags.
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///    http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+var Charts;
+(function (Charts) {
+    'use strict';
+    /// NOTE: this pattern is used because enums cant be used with strings
+    var EventNames = (function () {
+        function EventNames(value) {
+            this.value = value;
+            // empty
+        }
+        EventNames.prototype.toString = function () {
+            return this.value;
+        };
+        EventNames.CHART_TIMERANGE_CHANGED = new EventNames('ChartTimeRangeChanged');
+        EventNames.AVAIL_CHART_TIMERANGE_CHANGED = new EventNames('AvailChartTimeRangeChanged');
+        EventNames.REFRESH_CHART = new EventNames('RefreshChart');
+        EventNames.REFRESH_AVAIL_CHART = new EventNames('RefreshAvailabilityChart');
+        return EventNames;
+    })();
+    Charts.EventNames = EventNames;
 })(Charts || (Charts = {}));
 
 /// <reference path='../../vendor/vendor.d.ts' />
